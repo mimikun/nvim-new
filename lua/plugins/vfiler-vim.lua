40a876514b2c938de0e8f0897b08dcef8501dc96
Yuto Tanaka <karaage_nennen@icloud.com>
2024-05-23 21:57:43 +0900

    update

--- lua/plugins/vfiler-vim.lua
+++ lua/plugins/vfiler-vim.lua
@@ -1,19 +1,29 @@
--- TODO: setting
+---@type table
+local dependencies = {
+    "obaland/vfiler-column-devicons",
+}
 
 ---@type LazySpec
 local spec = {
     "obaland/vfiler.vim",
-    lazy = false,
+    --lazy = false,
     cmd = "VFiler",
-    --keys = "",
-    --event = "VeryLazy",
-    --dependencies = { "nvim-tree/nvim-web-devicons" },
-    --opts = {},
-    --config = true,
+    dependencies = dependencies,
+    config = function()
+        local vfiler_config = require("vfiler/config")
+        vfiler_config.setup({
+            options = {
+                auto_cd = true,
+                auto_resize = true,
+                keep = true,
+                layout = "left",
+                name = "explorer",
+                width = 30,
+                columns = "indent,devicons,name",
+            },
+        })
+    end,
     --cond = false,
 }
 
 return spec
---[[
-
-]]
