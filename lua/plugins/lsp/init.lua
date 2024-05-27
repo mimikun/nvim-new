15d3ec0509c83689f79eb8f55996c67c41c77c81
Yuto Tanaka <karaage_nennen@icloud.com>
2024-05-27 23:36:35 +0900

    rename

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -3,6 +3,9 @@ local spec = {
     { import = "plugins.lsp.lspsaga" },
     { import = "plugins.lsp.nvim-lspconfig" },
     { import = "plugins.lsp.garbage-day" },
+    -- TODO:
+    { import = "plugins.lsp.null-ls" },
+
 }
 
 return spec
--- lua/plugins/lsp/null-ls.lua
+++ lua/plugins/lsp/null-ls.lua
@@ -1,11 +1,5 @@
-local spec = {
-    -- TODO:
-}
-
-return spec
---[[
-local global = require("core.global")
-local is_windows = global.is_windows
+---@type boolean
+local is_windows = require("config.global").is_windows
 
 ---@type LazySpec
 local spec = {
@@ -13,7 +7,6 @@ local spec = {
     lazy = false,
     --event = "VeryLazy",
     dependencies = { "nvim-lua/plenary.nvim" },
-    -- TODO: settings
     config = function()
         local null_ls = require("null-ls")
         local code_actions = null_ls.builtins.code_actions
@@ -52,6 +45,7 @@ local spec = {
             --formatting.prettier,
             --formatting.yamlfmt,
         }
+
         if is_windows then
             for i, v in ipairs(sources) do
                 if (v == "formatting.shfmt") or (v == "diagnostics.actionlint") or (v == "diagnostics.checkmake") then
@@ -66,4 +60,3 @@ local spec = {
 }
 
 return spec
-]]
