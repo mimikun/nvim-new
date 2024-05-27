3c45de5c462ff431e49d523102c8cdf4e1c0f371
Yuto Tanaka <karaage_nennen@icloud.com>
2024-05-27 23:34:19 +0900

    wp

--- lua/plugins/lsp/garbage-day.lua
+++ lua/plugins/lsp/garbage-day.lua
@@ -1,18 +1,11 @@
-local spec = {
-    -- TODO:
-}
-
-return spec
---[[
 ---@type LazySpec
 local spec = {
     "zeioth/garbage-day.nvim",
     --lazy = false,
     event = "VeryLazy",
     dependencies = { "neovim/nvim-lspconfig" },
-    config = true,
+    opts = {},
     --cond = false,
 }
 
 return spec
-]]
--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -2,6 +2,7 @@
 local spec = {
     { import = "plugins.lsp.lspsaga" },
     { import = "plugins.lsp.nvim-lspconfig" },
+    { import = "plugins.lsp.garbage-day" },
 }
 
 return spec
