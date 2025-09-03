555ffb1a1e79f454ef70c1d6a84e79318cb06b57
Yuto Tanaka <git@mimikun.dev>
2025-09-03 18:14:09 +0900

    update noice-nvim

--- lua/plugins/noice-nvim/opts.lua
+++ lua/plugins/noice-nvim/opts.lua
@@ -15,7 +15,7 @@ local opts = {
     bottom_search = true, -- use a classic bottom cmdline for search
     command_palette = true, -- position the cmdline and popupmenu together
     long_message_to_split = true, -- long messages will be sent to a split
-    inc_rename = false, -- enables an input dialog for inc-rename.nvim
+    inc_rename = true,
     lsp_doc_border = false, -- add a border to hover docs and signature help
     cmdline_output_to_split = false, -- send the output of a command you executed in the cmdline to a split
   },
