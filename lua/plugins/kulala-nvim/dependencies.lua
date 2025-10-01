9c3abab78a4a29320cbff8d5df0ec2d297db54a4
Yuto Tanaka <git@mimikun.dev>
2025-10-01 17:57:56 +0900

    feat(kulala-nvim): add telescope-kulala.nvim

---
+++ lua/plugins/kulala-nvim/dependencies.lua
@@ -0,0 +1,8 @@
+---@type LazySpec[]
+local dependencies = {
+  "nvim-telescope/telescope.nvim",
+  "mikelue/telescope-kulala.nvim",
+  { "nvim-treesitter/nvim-treesitter", branch = "main" },
+}
+
+return dependencies
--- lua/plugins/kulala-nvim/init.lua
+++ lua/plugins/kulala-nvim/init.lua
@@ -5,8 +5,23 @@ local spec = {
   ft = require("plugins.kulala-nvim.ft"),
   keys = require("plugins.kulala-nvim.keys"),
   --event = "VeryLazy",
-  dependencies = { "nvim-treesitter/nvim-treesitter" },
-  opts = require("plugins.kulala-nvim.opts"),
+  dependencies = require("plugins.kulala-nvim.dependencies"),
+  config = function()
+    local telescope = require("telescope")
+
+    require("kulala").setup(require("plugins.kulala-nvim.opts"))
+
+    telescope.setup({
+      extensions = {
+        kulala = {
+          method_width = 8,
+          url_width = 256,
+          desc_width = 32,
+        },
+      },
+    })
+    telescope.load_extension("kulala")
+  end,
   cond = false,
   enabled = false,
 }
